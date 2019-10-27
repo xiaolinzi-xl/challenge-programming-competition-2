ce27e09464232e1cca9c6a9ff88af4cb468519a4
xiaolinzi-xl <daoshangxl@163.com>
2019-10-27 15:01:39 +0800

    ALDS1_6_A 计数排序 标准解法

--- chapter_7/counting_sort.py
+++ chapter_7/counting_sort.py
@@ -11,10 +11,27 @@ def count_sort(arr, max_element=10000):
             c[i] -= 1
 
 
+def count_sort_2(arr, max_element=10000):
+    c = [0] * (max_element + 1)
+
+    for ele in arr:
+        c[ele] += 1
+    for i in range(1, max_element + 1):
+        c[i] += c[i - 1]
+
+    arr_copy = list(arr)
+    n = len(arr)
+    # 从后往前选择元素是为了保证排序算法的稳定性
+    for i in range(n - 1, -1, -1):
+        arr[c[arr_copy[i]] - 1] = arr_copy[i]
+        c[arr_copy[i]] -= 1
+
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
-    count_sort(arr)
+    # count_sort(arr)
+    count_sort_2(arr)
 
     arr = map(str, arr)
     print(' '.join(arr))
